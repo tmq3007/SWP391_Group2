eeb0a329203b09d97988ceeba67659f1db70c1a1
tmq3007 <quantm3007@gmail.com>
2024-09-05 22:20:33 +0700

    1st commit

---
+++ src/test/java/com/shoppingapp/shoppingapp/ShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingapp.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
