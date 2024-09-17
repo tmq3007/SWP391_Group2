7a726fb2121d0f67c19a9d19ac2c645799b713fb
aronoob <39618559+mine1221@users.noreply.github.com>
2024-09-17 10:03:04 +0700

    commit all user crud

---
+++ src/main/java/com/shoppingapp/shoppingapp/service/UserService.java
@@ -0,0 +1,21 @@
+package com.shoppingapp.shoppingapp.service;
+
+import com.shoppingapp.shoppingapp.models.User;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+
+    User addUser(User user);
+
+    User updateUser(User User, Long id);
+
+    String deleteUser(Long id);
+
+
+}
