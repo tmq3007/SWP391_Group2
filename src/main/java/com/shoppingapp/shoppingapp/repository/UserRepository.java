a7a3e4d169e96f78b1b4ab4c45fe5495de62418d
aronoob <39618559+mine1221@users.noreply.github.com>
2024-09-17 10:03:04 +0700

    commit all user crud

--- src/main/java/com/shoppingapp/shoppingapp/repository/UserRepository.java
+++ src/main/java/com/shoppingapp/shoppingapp/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.shoppingapp.shoppingapp.repository;
 
-public interface UserRepository {
+import com.shoppingapp.shoppingapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
