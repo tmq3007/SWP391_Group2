a6430aedb4576320d3b08d5f38460ac2c9649f22
tmq3007 <quantm3007@gmail.com>
2024-09-17 08:56:36 +0700

    commit update category and delete category

--- src/main/java/com/shoppingapp/shoppingapp/controllers/CategoryController.java
+++ src/main/java/com/shoppingapp/shoppingapp/controllers/CategoryController.java
@@ -31,6 +31,28 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.addCategory(category)) ;
     }
 
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity<Category> updateCategory(@RequestBody Category category,
+                                                   @PathVariable("categoryId") Long categoryId){
+        Category categoryObj = categoryService.getCategory(categoryId);
+        if(categoryObj != null){
+            categoryObj.setCategoryName(category.getCategoryName());
+            categoryObj.setDescription(category.getDescription());
+            categoryObj.setPicture(category.getPicture());
+            categoryObj.setIsActive(category.getIsActive());
+            categoryService.updateCategory(categoryObj);
+        }
+        return ResponseEntity.ok(categoryService.updateCategory(categoryObj));
+    }
 
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable("categoryId") Long categoryId) {
+        Category categoryObj = categoryService.getCategory(categoryId);
+        String deleteMsg = "";
+        if(categoryObj != null){
+            deleteMsg = categoryService.deleteCategory(categoryObj);
+        }
+        return ResponseEntity.ok(deleteMsg);
+    }
 
 }
