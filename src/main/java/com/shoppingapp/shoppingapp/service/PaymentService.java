179613ddf2274219a5413685a0fe895dfa996950
tmq3007 <quantm3007@gmail.com>
2024-09-21 14:50:53 +0700

    add Payment Response

--- src/main/java/com/shoppingapp/shoppingapp/service/PaymentService.java
+++ src/main/java/com/shoppingapp/shoppingapp/service/PaymentService.java
@@ -2,14 +2,16 @@ package com.shoppingapp.shoppingapp.service;
 
 import com.shoppingapp.shoppingapp.dto.request.PaymentCreationRequest;
 import com.shoppingapp.shoppingapp.dto.request.PaymentUpdateRequest;
+import com.shoppingapp.shoppingapp.dto.response.PaymentResponse;
 import com.shoppingapp.shoppingapp.models.Payment;
 
 import java.util.List;
 
 public interface PaymentService {
     List<Payment> getAllPayments();
-    Payment getPayment(Long id);
+    PaymentResponse getPayment(Long id);
+    Payment getPaymentById(Long id);
     Payment addPayment(PaymentCreationRequest request);
-    Payment updatePayment(PaymentUpdateRequest request, Long id);
+    PaymentResponse updatePayment(PaymentUpdateRequest request, Long id);
     String deletePayment(Payment payment);
 }
