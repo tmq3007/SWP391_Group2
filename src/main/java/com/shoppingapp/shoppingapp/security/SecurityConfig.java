26ae7b1291e39d2bb3f81e45375430499664ad51
tmq3007 <quantm3007@gmail.com>
2024-10-03 12:01:14 +0700

    relationship between admin,vendor,user, shop, product, category

--- src/main/java/com/shoppingapp/shoppingapp/security/SecurityConfig.java
+++ src/main/java/com/shoppingapp/shoppingapp/security/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
                         "http://localhost:3000",
                         "http://localhost:3001",
                         "http://localhost:3007",
-                        "http://localhost:3003"
+                        "http://localhost:3004"
                 ));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
