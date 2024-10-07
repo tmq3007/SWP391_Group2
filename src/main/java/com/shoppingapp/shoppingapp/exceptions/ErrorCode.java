e6ef2ff361c79563d7d736aab738e2d48d0d218b
aronoob <39618559+mine1221@users.noreply.github.com>
2024-10-07 07:37:36 +0700

    Update login

--- src/main/java/com/shoppingapp/shoppingapp/exceptions/ErrorCode.java
+++ src/main/java/com/shoppingapp/shoppingapp/exceptions/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatusCode;
 @Getter
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_EXISTED(1000, "Username existed", HttpStatus.BAD_REQUEST),
     INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
     USERNAME_INVALID(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
@@ -23,6 +24,7 @@ public enum ErrorCode {
     PRODUCT_NOT_EXISTED(1012, "Product not existed", HttpStatus.BAD_REQUEST),
     SHOP_EXISTED(1013, "Shop existed", HttpStatus.BAD_REQUEST),
     SHOP_NOT_EXISTED(1014, "Shop not existed", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD_CONFIRM(1015, "Password is incorrect", HttpStatus.BAD_REQUEST),
     ORDER_NOT_EXISTED(2000,"ORDER NOT EXITED",HttpStatus.BAD_REQUEST),
     ;
 
