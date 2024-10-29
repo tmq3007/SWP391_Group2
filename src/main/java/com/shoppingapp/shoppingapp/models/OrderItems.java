6e8e3af341b50caa79239ccd2609604ae32dbf41
LAPTOP <duongnthe180080@fpt.edu.vn>
2024-10-30 01:18:36 +0700

    addition to get orderitems from shop

--- src/main/java/com/shoppingapp/shoppingapp/models/OrderItems.java
+++ src/main/java/com/shoppingapp/shoppingapp/models/OrderItems.java
@@ -6,34 +6,31 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.AccessType;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItems {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderItemsId;
 
     private String productName;
-
     private String productImage;
-
     private String productSellPrice;
-
     private Double discount;
-
     private Long productQuantity;
-
     private Double itemTotalPrice;
-
     private Double finalPrice;
 
-    @ManyToOne // by id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    @JsonBackReference
     private Orders orders;
 
-    @ManyToOne // by shop id
+    @ManyToOne
+    @JoinColumn(name = "shop_id")
     private Shop shop;
-
 }
