3e403f2eea4c0220e93955f5b230c7eaee27fc13
aronoob <39618559+mine1221@users.noreply.github.com>
2024-10-30 22:33:28 +0700

    Delete unused file

--- src/main/java/com/shoppingapp/shoppingapp/mapper/OrdersMapper.java
+++ src/main/java/com/shoppingapp/shoppingapp/mapper/OrdersMapper.java
@@ -1,12 +1,6 @@
 package com.shoppingapp.shoppingapp.mapper;
 
-import com.shoppingapp.shoppingapp.dto.request.OrdersCreationRequest;
-import com.shoppingapp.shoppingapp.dto.request.OrdersUpdateRequest;
-import com.shoppingapp.shoppingapp.dto.response.OrdersResponse;
-import com.shoppingapp.shoppingapp.models.Orders;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface OrdersMapper {
