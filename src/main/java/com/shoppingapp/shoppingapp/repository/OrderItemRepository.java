80c1d990dbcca989818cb85c47d322a308bacb4f
aronoob <39618559+mine1221@users.noreply.github.com>
2024-10-30 17:54:46 +0700

    Change in get statistic of shops

--- src/main/java/com/shoppingapp/shoppingapp/repository/OrderItemRepository.java
+++ src/main/java/com/shoppingapp/shoppingapp/repository/OrderItemRepository.java
@@ -2,9 +2,14 @@ package com.shoppingapp.shoppingapp.repository;
 
 import com.shoppingapp.shoppingapp.models.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItems, Long> {
+
+    @Query(value = "SELECT COUNT(*) FROM orders WHERE shop_id = :shopId", nativeQuery = true)
+    int countOrderItemsByShopId(@Param("shopId") Long shopId);
 }
